4713eb8e990d5f69748ce8d3c75c0b3faac7b283
Leo <leo.lu@orderplus.com>
2022-06-13 14:00:43 +0800

    deploy

--- assets/pic.81ee443d.js
+++
@@ -1,1 +0,0 @@
-import{b9 as p}from"./runtime-dom.esm-bundler.4b067fc9.js";const a={};p(a).mount("#app");
---
+++ assets/pic.8ac282c2.js
@@ -0,0 +1,1 @@
+import{e as p}from"./runtime-dom.esm-bundler.79609ac0.js";const a={};p(a).mount("#app");
